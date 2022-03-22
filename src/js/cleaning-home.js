fdea3759f0fd6475230be2619c63fa848e481274
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-03-22 16:41:31 +0100

    Finished Cleaning model for use in other files.

--- src/js/cleaning-home.js
+++ src/js/cleaning-home.js
@@ -1,44 +1,39 @@
-let currentCleanings;
+import Cleaning from "./models/Cleaning.js";
+
+let currentCleanings = [];
 
 window.onload = () => {
-    initializeModal();
-    currentCleanings = fetchCleaning();
+    initializeForm();
+    currentCleanings = Cleaning.fetchCleanings();
     console.log(currentCleanings);
 };
 
-function initializeModal() {
-    let modal = document.getElementById("new-cleaning-modal");
-
-    let modalForm = document.getElementById("new-cleaning-modal-form");
-    let closeModalButton = document.getElementById("new-cleaning-modal-close");
-    let confirmModalButton = document.getElementById(
-        "new-cleaning-modal-confirm"
-    );
-    let openModalButton = document.getElementById("new-cleaning");
-
-    openModalButton.addEventListener("click", () => {
-        modal.classList.remove("hidden");
-        modal.classList.add("visible");
-    });
-
-    closeModalButton.addEventListener("click", () => {
-        modal.classList.remove("visible");
-        modal.classList.add("hidden");
-    });
-
-    confirmModalButton.addEventListener("click", (e) => {
-        modalForm.reportValidity();
-
-        if (modalForm.checkValidity()) {
-            console.log(true);
-        } else {
-            console.log(false);
+function initializeForm() {
+    let cleaningForm = document.getElementById("new-cleaning-form");
+    let confirmFormButton = document.getElementById("new-cleaning-confirm");
+    confirmFormButton.addEventListener("click", () => {
+        cleaningForm.reportValidity();
+        if (cleaningForm.checkValidity()) {
+            addNewCleaning();
         }
     });
 }
 
-function fetchCleaning() {
-    let storedCleanings = JSON.parse(localStorage.getItem("cleanings"));
-    if (storedCleanings === null) return [];
-    return storedCleanings;
+function addNewCleaning() {
+    let customerTag = document.getElementById("customer");
+    let dateTag = document.getElementById("date");
+    let timeTag = document.getElementById("time");
+    let descriptionTag = document.getElementById("description");
+    let newCleaning = new Cleaning({
+        customer: customerTag.value,
+        dateAndTimeString: new Date(
+            dateTag.value + " " + timeTag.value
+        ).toString(),
+        description: descriptionTag.value,
+        finished: false,
+        acceptedByCustomer: false,
+    });
+    currentCleanings.push(newCleaning);
+    Cleaning.storeCleanings(currentCleanings);
+    console.log(currentCleanings);
 }
