25f6ab688ac184805fea6747a14947a2d6103e07
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-03-23 14:30:01 +0100

    Added integration of User class into creation of Cleaning instances.

--- src/js/cleaning-home.js
+++ src/js/cleaning-home.js
@@ -1,11 +1,32 @@
 import Cleaning from "./models/Cleaning.js";
+import User from "./models/User.js";
+import { ROLE } from "./models/Role.js";
 
 let currentCleanings = [];
+let currentUsers = [];
 
 window.onload = () => {
-    initializeForm();
     currentCleanings = Cleaning.fetchCleanings();
-    console.log(currentCleanings);
+    currentUsers = User.fetchUsers();
+    initializeForm();
+    console.log("CLEANINGS: ", currentCleanings);
+    console.log("USERS: ", currentUsers);
+
+    // let testUser = new User({
+    //     username: "Kalle",
+    //     password: "123",
+    //     role: ROLE.CUSTOMER,
+    // });
+
+    // let testCleaner = new User({
+    //     username: "Albert",
+    //     password: "123",
+    //     role: ROLE.CLEANER,
+    // });
+
+    // currentUsers.push(testUser);
+    // currentUsers.push(testCleaner);
+    // User.storeUsers(currentUsers);
 };
 
 function initializeForm() {
@@ -17,15 +38,30 @@ function initializeForm() {
             addNewCleaning();
         }
     });
+
+    let selectCustomer = document.getElementById("customer");
+    let selectCleaner = document.getElementById("cleaner");
+    currentUsers.forEach((user) => {
+        let optionTag = document.createElement("option");
+        optionTag.value = user.username;
+        optionTag.innerText = user.username;
+        if (user.role === ROLE.CLEANER) {
+            selectCleaner.appendChild(optionTag);
+        } else if (user.role === ROLE.CUSTOMER) {
+            selectCustomer.appendChild(optionTag);
+        }
+    });
 }
 
 function addNewCleaning() {
     let customerTag = document.getElementById("customer");
+    let cleanerTag = document.getElementById("cleaner");
     let dateTag = document.getElementById("date");
     let timeTag = document.getElementById("time");
     let descriptionTag = document.getElementById("description");
     let newCleaning = new Cleaning({
-        customer: customerTag.value,
+        customer: customerTag.options[customerTag.selectedIndex].value,
+        cleaner: cleanerTag.options[cleanerTag.selectedIndex].value,
         dateAndTimeString: new Date(
             dateTag.value + " " + timeTag.value
         ).toString(),
@@ -35,5 +71,7 @@ function addNewCleaning() {
     });
     currentCleanings.push(newCleaning);
     Cleaning.storeCleanings(currentCleanings);
-    console.log(currentCleanings);
+
+    let aTagHome = document.getElementById("home-a-tag");
+    aTagHome.click();
 }
