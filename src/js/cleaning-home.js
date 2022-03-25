b7bddb4c9627e1a768bbb5d2b054f74a62fea427
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-03-25 16:07:37 +0100

    Finished modal and recipt page

--- src/js/cleaning-home.js
+++ src/js/cleaning-home.js
@@ -4,6 +4,7 @@ import { ROLE } from "./models/Role.js";
 
 let currentCleanings = [];
 let currentUsers = [];
+let validInput = false;
 
 window.onload = () => {
     currentCleanings = Cleaning.fetchCleanings();
@@ -35,8 +36,10 @@ function initializeForm() {
     let confirmFormButton = document.getElementById("new-cleaning-confirm");
     confirmFormButton.addEventListener("click", () => {
         cleaningForm.reportValidity();
+        validInput = false;
         if (cleaningForm.checkValidity()) {
-            addNewCleaning();
+            createNewCleaning();
+            validInput = true;
         }
     });
 
@@ -54,7 +57,7 @@ function initializeForm() {
     });
 }
 
-function addNewCleaning() {
+function createNewCleaning() {
     let customerTag = document.getElementById("customer");
     let cleanerTag = document.getElementById("cleaner");
     let dateTag = document.getElementById("date");
@@ -81,13 +84,12 @@ function addNewCleaning() {
 
 function initializeModal() {
     let modalDeny = document.getElementById("modal-deny");
-    let modalConfirm = document.getElementById("modal-confirm");
     let modal = document.getElementById("cleaning-modal");
 
     modalDeny.addEventListener("click", () => {
+        validInput = false;
         modal.classList.remove("modal-in");
         modal.classList.add("modal-out");
-
         setTimeout(() => {
             modal.classList.remove("visible");
             modal.classList.add("hidden");
@@ -104,4 +106,60 @@ function triggerModal(newCleaning) {
     modal.classList.add("visible");
 
     let innerModalContainer = document.getElementById("__inner-modal-content");
+    innerModalContainer.innerHTML = "";
+
+    let h3 = document.createElement("h3");
+    h3.innerText = "Kontrollera informationen";
+    innerModalContainer.appendChild(h3);
+
+    let ul = document.createElement("ul");
+
+    ul.appendChild(createLiEntry("Kund:", newCleaning.customer));
+    ul.appendChild(
+        createLiEntry(
+            "Datum: ",
+            new Date(newCleaning.dateAndTimeString).toLocaleDateString()
+        )
+    );
+    ul.appendChild(
+        createLiEntry(
+            "När:",
+            new Date(newCleaning.dateAndTimeString).toLocaleTimeString()
+        )
+    );
+    ul.appendChild(createLiEntry("Städare:", newCleaning.cleaner));
+    innerModalContainer.appendChild(ul);
+
+    let descriptionLabel = document.createElement("h4");
+    descriptionLabel.innerText = "Beskrivning";
+    descriptionLabel.className = "description";
+    innerModalContainer.appendChild(descriptionLabel);
+
+    let descriptionTag = document.createElement("span");
+    descriptionTag.innerText = newCleaning.description;
+    descriptionTag.className = "description";
+
+    innerModalContainer.appendChild(descriptionTag);
+
+    document.getElementById("modal-confirm").addEventListener("click", (e) => {
+        e.preventDefault();
+
+        if (validInput) {
+            currentCleanings.push(newCleaning);
+            Cleaning.storeCleanings(currentCleanings);
+            localStorage.setItem("recipt", JSON.stringify(newCleaning));
+            window.location.href = e.target.href;
+        }
+    });
+}
+
+function createLiEntry(label, text) {
+    let li = document.createElement("li");
+    let labelTag = document.createElement("span");
+    let textTag = document.createElement("span");
+    labelTag.innerText = label;
+    textTag.innerText = text;
+    li.appendChild(labelTag);
+    li.appendChild(textTag);
+    return li;
 }
